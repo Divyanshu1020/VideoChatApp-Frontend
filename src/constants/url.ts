337ab9f7e83887b04663b49782a1cb16982ff3af
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-09-11 02:51:46 +0530

    ✅ write some constants

---
+++ src/constants/url.ts
@@ -0,0 +1,30 @@
+
+
+export default {
+
+    // user
+    getUserData : "user/get-my-Profile",
+    logout: "user/logout",
+    register: "user/register",
+    login: "user/login",
+    userSearch:"user/search",
+    sendFriendRequest: "user/request/send",
+    acceptOrDecline: "user/request/accept",
+    getNotification : "user/notifications",
+
+    // group
+    createGroup: "chat/group/create",
+    leaveGroup: "chat/group/member/leave",
+    myGroup: "chat/group/my",
+    addMemeberInGroup: "chat/group/member/add",
+    removeMemberInGroup: "chat/group/member/remove",
+    renameGroup: "chat/group/rename",
+
+    // chat
+    allChats: "chat/all",
+    friends: "chat/friends",
+
+    attachment : "chat/attachment",
+    chatAllMessage: "chat/messages/",
+    chatDetails: "chat/details/",
+}
